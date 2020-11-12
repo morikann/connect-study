85dde8813d06e86e5b5c8a0ff9b4287dfa0795f4
morikann <mrknt0088@gmail.com>
2020-11-12 09:38:59 +0900

    devise の設定
    close #9

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'home#index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  root to: 'homes#index'
 end
