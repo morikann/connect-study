55f05aa3e6dda0c947b631a1617125578503f935
morikann <mrknt0088@gmail.com>
2020-11-17 10:39:09 +0900

    プロフィール画像の追加
    close #27

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_064822) do
+ActiveRecord::Schema.define(version: 2020_11_17_001124) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_064822) do
     t.string "prefecture_id"
     t.text "profile"
     t.text "aim"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
