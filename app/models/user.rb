2afaa47b62021a1fa4148693ea3cb9cee7065f3a
morikann <mrknt0088@gmail.com>
2020-11-25 21:17:00 +0900

    フォロー機能のモデルの実装
    close #44

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,25 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  # mount_uploader :avatar, AvatarUploader
-
   has_one :profile, dependent: :destroy
+
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id, dependent: :destroy
+  has_many :following, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id, dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :following
+
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(follower_id: other_user.id).destroy 
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 end
