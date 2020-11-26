6b1c46bb5a8c1aa4281014a5fb5c882e1fe70926
morikann <mrknt0088@gmail.com>
2020-11-26 17:29:55 +0900

    フォロー機能のAjax化
    close #48

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,15 +1,21 @@
 class RelationshipsController < ApplicationController
 
   def create
-    user = User.find(params[:follower_id])
-    current_user.follow(user)
-    redirect_to user.profile
+    @user = User.find(params[:follower_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user.profile }
+      format.js
+    end
   end
 
   def destroy 
-    user = Relationship.find(params[:id]).follower
-    current_user.unfollow(user)
-    redirect_to user.profile
+    @user = Relationship.find(params[:id]).follower
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user.profile }
+      format.js
+    end
   end
   
 end
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module ConnectStudy
     config.i18n.default_locale =:ja
     # エラーが発生した際に付与される field_with_errors クラスを読み込まないようにする
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    # 認証トークンをリモートフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
