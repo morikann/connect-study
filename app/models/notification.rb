5bc1bafce9474a7f974172d3fd7ba8fd24d05218
morikann <mrknt0088@gmail.com>
2020-11-29 19:43:18 +0900

    通知画面の作成
    close #58

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :visitor, class_name: "User", optional: true
   belongs_to :visited, class_name: "User", optional: true
+
+  default_scope -> { order(created_at: :desc) }
 end
