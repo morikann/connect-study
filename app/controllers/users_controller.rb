1037acfc2392908893b2ffbba1699858c9bde872
morikann <mrknt0088@gmail.com>
2020-12-07 15:41:43 +0900

    フォロー、フォロワー一覧ページの作成
    close #84

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,42 +1,18 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
-  # def new
-  #   @user_profile = User.new
-  # end
-
-  # def create 
-  #   @user_profile = User.new(profile_params)
-
-  #   if @user_profile.save
-  #     flash[:notice] = "プロフィールの設定が完了しました"
-  #     redirect_to root_url
-  #   else
-  #     render 'new'
-  #   end
-  # end
-
-  def edit 
-    @user_profile = User.find(params[:id])
+  def following
+    @title = 'フォロー中'
+    @user = User.find(params[:id])
+    @users = @user.following.includes(profile: :tags).page(params[:page])
+    render 'show_follow'
   end
 
-  def update 
-    @user_profile = User.find(params[:id])
-    if @user_profile.update(profile_params)
-      flash[:notice] = "プロフィールの設定が完了しました"
-      redirect_to root_url
-    else
-      render 'edit'
-    end
+  def followers
+    @title = 'フォロワー'
+    @user = User.find(params[:id])
+    @users = @user.followers.includes(profile: :tags).page(params[:page])
+    render 'show_follow'
   end
 
-  def show
-
-  end
-
-  private
-
-  def profile_params
-    params.require(:user).permit(:aim, :profile, :birth_date, :username, :prefecture_id, :avatar)
-  end
 end
