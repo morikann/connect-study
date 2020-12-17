493ab953dbbd86ab7e2907cb0422104cf9b22ee4
morikann <mrknt0088@gmail.com>
2020-12-17 13:37:11 +0900

    testが生成されないように設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module ConnectStudy
   class Application < Rails::Application
+    # testを生成されないようにする
+    config.generators do |g|
+      g.test_framework false
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
