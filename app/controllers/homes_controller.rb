a04c3adaad85cd7ac7efc98a6051bae0f7491ee5
morikann <mrknt0088@gmail.com>
2020-12-17 13:31:26 +0900

    UIの変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,12 +1,4 @@
 class HomesController < ApplicationController
   skip_before_action :authenticate_user! 
 
-  def index
-    if params[:search].present?
-      @users = User.includes(profile: :tags).search(params[:search]).page(params[:page])
-    else
-      @users = User.includes(profile: :tags).page(params[:page])
-    end
-    # @user_profile = current_user.profile if current_user.present?
-  end
 end
