d08a9c5bd03c1f2abcab17ece40a4c8e41b13365
morikann <mrknt0088@gmail.com>
2020-12-25 12:38:24 +0900

    simple_calendarと勉強会を連携させる

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,7 @@
 class HomesController < ApplicationController
   skip_before_action :authenticate_user! 
 
+  def index 
+    @study_events = StudyEvent.joins(:event_users).where(event_users: {user_id: current_user.id})
+  end
 end
