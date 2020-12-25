fcc10a9dfc93acfff8072e0aadc83c749f2368d3
morikann <mrknt0088@gmail.com>
2020-12-25 12:44:19 +0900

    ユーザーがログインしている時のみ@study_eventsインスタンスを取得する

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,6 +2,8 @@ class HomesController < ApplicationController
   skip_before_action :authenticate_user! 
 
   def index 
-    @study_events = StudyEvent.joins(:event_users).where(event_users: {user_id: current_user.id})
+    if signed_in?
+      @study_events = StudyEvent.joins(:event_users).where(event_users: {user_id: current_user.id})
+    end
   end
 end
