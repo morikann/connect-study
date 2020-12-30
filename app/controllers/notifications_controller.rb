b72631cef3b2cc8a4168c7dce9c1dba06d574411
morikann <mrknt0088@gmail.com>
2020-12-30 19:55:31 +0900

    勉強会への参加を希望する通知を主催者に送る機能を実装

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,4 +5,39 @@ class NotificationsController < ApplicationController
       notification.update(checked: true)
     end
   end
+
+  def create 
+    duplicate_notification = Notification.where(
+      visitor_id: current_user.id,
+      study_event_id: notification_params[:study_event_id], 
+      action: 'attend_request'
+    )
+    # 同じ勉強会への参加申請が連続して送信された場合リダイレクトさせる
+    if duplicate_notification.exists?
+      flash[:alert] = "すでに参加希望の申請は完了しています"
+      return redirect_to study_event_path(notification_params[:study_event_id])
+    end
+
+    notification = current_user.active_notifications.build(notification_params)
+    event_user = EventUser.where(user_id: current_user.id, study_event_id: notification_params[:study_event_id])
+    # 申し込むユーザーがすでに勉強会に参加しているかどうか判定
+    if event_user.blank?
+      if notification.save
+        flash[:notice] = "参加希望の申請が完了しました"
+        redirect_to study_event_path(notification_params[:study_event_id])
+      else
+        flash[:alert] = "参加希望の申請に失敗しました"
+        redirect_to study_event_path(notification_params[:study_event_id])
+      end
+    else
+      flash[:alert] = "すでに参加しています"
+      redirect_to study_event_path(notification_params[:study_event_id])
+    end
+  end
+
+  private
+
+  def notification_params
+    params.require(:notification).permit(:visited_id, :action, :study_event_id)
+  end
 end
