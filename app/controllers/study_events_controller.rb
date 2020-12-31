40304692188bc3be6598225842b93a6d219375fb
morikann <mrknt0088@gmail.com>
2020-12-31 13:36:55 +0900

    step登録のコードの変更
    study_eventのdescriptionカラムが文字数によってはsession overflowを引き起こす可能性があるのでstudy_eventのパラメータに関してはhidden_fieldで渡す。
    imageカラムに関してはcarrierwaveを用いているためキャッシュから復元する必要があるなど複雑になっているのでsessionで管理。

--- app/controllers/study_events_controller.rb
+++ app/controllers/study_events_controller.rb
@@ -3,8 +3,9 @@ class StudyEventsController < ApplicationController
     @study_events = StudyEvent.includes(:location, :tags, user: :profile).search(search_params)
   end
 
-  def new 
-    @study_event = current_user.my_study_events.build(session[:study_event] || {})
+  def new  #step1
+    # 初めての入力か、戻るボタンからきたかを判断してインスタンスをつくる
+    params[:study_event] ? @study_event = StudyEvent.new(study_event_params) : @study_event = StudyEvent.new
   end
 
   def edit
@@ -16,19 +17,20 @@ class StudyEventsController < ApplicationController
     @study_event = current_user.my_study_events.build(study_event_params)
     if @study_event.valid?
       # フォームで渡された値のみ保存。ActionController::Parametersの仕様変更の可能性があるのでstudy_event_paramsをそのまま保存しない。
-      session[:study_event] = @study_event.attributes.slice(*study_event_params.keys)
-      # インスタンスの属性にはparamsと異なる形式の日付、時間が格納されているのでその形式を元に戻す
-      session[:study_event]['date'] = session[:study_event]['date'].strftime("%Y-%m-%d")
-      session[:study_event]['begin_time'] = session[:study_event]['begin_time'].strftime("%H:%M")
-      session[:study_event]['finish_time']= session[:study_event]['finish_time'].strftime("%H:%M")
-      session[:study_event]['tag'] = study_event_params[:tag]
+      # session[:study_event] = @study_event.attributes.slice(*study_event_params.keys)
+      # # インスタンスの属性にはparamsと異なる形式の日付、時間が格納されているのでその形式を元に戻す
+      # session[:study_event]['date'] = session[:study_event]['date'].strftime("%Y-%m-%d")
+      # session[:study_event]['begin_time'] = session[:study_event]['begin_time'].strftime("%H:%M")
+      # session[:study_event]['finish_time']= session[:study_event]['finish_time'].strftime("%H:%M")
+      # session[:study_event]['tag'] = study_event_params[:tag]
       
-      # 画像の変更があった場合のみ
+      # # 画像の変更があった場合のみ
       if @study_event.image.cache_name
         session[:image_cache_name] = @study_event.image.cache_name
         session[:image_url] = @study_event.image.url
       end
-      redirect_to step2_path
+      # get '/step2', to: locations#new
+      redirect_to step2_path(study_event: study_event_params)
     else
       render 'new'
     end
@@ -60,37 +62,46 @@ class StudyEventsController < ApplicationController
   end
 
   def event_user
+    # step3のurlを直接打ち込まれた際にエラーが生じないようにする
+    @study_event = StudyEvent.new(study_event_params) if params[:study_event]
     render 'event_user'
   end
 
   def confirm_event_user
     if params[:back]
-      return redirect_to step2_path
+      # get '/step2', to: locations#new
+      return redirect_to step2_path(study_event: study_event_params)
     end
     session[:event_users] = params[:user_ids].reject(&:blank?)
-    redirect_to confirm_path
+    redirect_to confirm_path(study_event: study_event_params)
   end
 
   def confirm 
+    # confirmのurlを直接打ち込まれた際にエラーが生じないようにする
+    if params[:study_event]
+    @study_event = StudyEvent.new(study_event_params)
+    @tag_list = study_event_params[:tag].split(/ |　/)
+    end
+
     gon.latitude = session[:location]['latitude'] if session[:location]
     gon.longitude = session[:location]['longitude'] if session[:location]
-    @tag_list = session[:study_event]['tag'].split(/ |　/) if session[:study_event]
+
     @prefecture = Prefecture.find(session[:location]['prefecture_id'].to_i).name if session[:location]
     render 'confirm'
   end
 
   def check_confirm
     if params[:back]
-      return redirect_to step3_path
+      return redirect_to step3_path(study_event: study_event_params)
     end
 
     location = Location.create(session[:location])
 
-    @study_event = current_user.my_study_events.build(session[:study_event])
+    @study_event = current_user.my_study_events.build(study_event_params)
     @study_event.location_id = location.id
     @study_event.image.retrieve_from_cache!(session[:image_cache_name]) if session[:image_cache_name]
-    @study_event.save
-    tag_list = session[:study_event]['tag'].split(/ |　/)
+    @study_event.save if @study_event.valid?
+    tag_list = study_event_params[:tag].split(/ |　/)
     @study_event.save_tags(tag_list)
     
     @study_event.event_users.create(user_id: current_user.id)
@@ -99,13 +110,12 @@ class StudyEventsController < ApplicationController
     # 招待したユーザーに通知を送る
     @study_event.create_notification_invite_user!(current_user, session[:event_users]) if session[:event_users]
 
-    session.delete(:study_event)
     session.delete(:location)
     session.delete(:image_cache_name)
     session.delete(:image_url)
     session.delete(:event_users)
 
-    redirect_to study_events_path
+    redirect_to study_events_path, notice: "勉強会「#{@study_event.name}」を作成しました"
   end
 
   private
