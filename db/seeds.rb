1b2bc3b7f37a4ed3ccd334d156b8f248c3b4c578
morikann <mrknt0088@gmail.com>
2021-01-01 15:53:21 +0900

    管理ユーザーの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,39 @@
-User.create!(
-  email: "example@example.com",
-  password: "password",
-  password_confirmation: "password"
+# 99.times do |n|
+#   email = "example#{n+1}@example.com"
+#   password = "password"
+#   User.create!(
+#     email: email,
+#     password: password,
+#     password_confirmation: password,
+#   )
+# end
+
+# User.all.each do |user|
+#   user.create_profile!(
+#     username: "ユーザー",
+#     purpose: "頑張るぞ",
+#     prefecture_id: 5,
+#     self_introduction: "日々精進",
+#     "birth_date(1i)"=>"1995",
+#     "birth_date(2i)"=>"6",
+#     "birth_date(3i)"=>"3",
+#   )
+# end
+
+admin_user = User.create(
+  email: 'admin@admin.com',
+  password: 'password',
+  password_confirmation: 'password',
+  admin: true
 )
 
-99.times do |n|
-  email = "example#{n+1}@example.com"
-  password = "password"
-  User.create!(
-    email: email,
-    password: password,
-    password_confirmation: password,
-  )
-end
-
-User.all.each do |user|
-  user.create_profile!(
-    username: "ユーザー",
-    purpose: "頑張るぞ",
-    prefecture_id: 5,
-    self_introduction: "日々精進",
-    "birth_date(1i)"=>"1995",
-    "birth_date(2i)"=>"6",
-    "birth_date(3i)"=>"3",
-  )
-end
+admin_user.create_profile!(
+  username: '管理ユーザー',
+  purpose: '良いサービスをつくる',
+  prefecture_id: 10,
+  self_introduction: 'connectを作りました',
+  "birth_date(1i)"=>"2000",
+  "birth_date(2i)"=>"8",
+  "birth_date(3i)"=>"8",
+  tag: 'プログラミング'
+)
