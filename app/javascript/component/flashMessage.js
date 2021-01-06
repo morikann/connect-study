9390dc18a47a8ab5580aea7c4a42de54c4fdb445
morikann <mrknt0088@gmail.com>
2021-01-06 17:44:34 +0900

    flashmessageが複数の場合も消えるように修正

--- app/javascript/component/flashMessage.js
+++ app/javascript/component/flashMessage.js
@@ -1,24 +1,26 @@
 document.addEventListener('turbolinks:load', () => {
   // クラスがflashMessageの要素を取得
-  const flashMessage = document.querySelector('.flash_message');
-  const deleteFlashMessage = () => {
-    // 20ミリ秒に一回 opacityを0.01減少させていき、0になったら非表示にしてインターバルを解除する
-    const id = setInterval(() => {
-      const opacity = flashMessage.style.opacity;
-
-      if (opacity > 0) {
-        flashMessage.style.opacity = opacity - 0.01;
-      } else {
-        flashMessage.style.display = 'none';
-        clearInterval(id);
-      }
-    }, 20);
-  };
-  // flashメッセージが存在する時のみ実行
-  if (flashMessage) {
-    // opacityの初期値を設定
-    flashMessage.style.opacity = 1;
-    // deleteFlashMessageを3000ミリ秒後に実行
-    setTimeout(deleteFlashMessage, 3000);
-  };
+  const flashMessage = document.querySelectorAll('.flash_message');
+    flashMessage.forEach((message) => {
+      const deleteFlashMessage = () => {
+        // 20ミリ秒に一回 opacityを0.01減少させていき、0になったら非表示にしてインターバルを解除する
+        const id = setInterval(() => {
+          const opacity = message.style.opacity;
+    
+          if (opacity > 0) {
+            message.style.opacity = opacity - 0.01;
+          } else {
+            message.style.display = 'none';
+            clearInterval(id);
+          }
+        }, 20);
+      };
+      // flashメッセージが存在する時のみ実行
+      if (message) {
+        // opacityの初期値を設定
+        message.style.opacity = 1;
+        // deleteFlashMessageを3000ミリ秒後に実行
+        setTimeout(deleteFlashMessage, 3000);
+      };
+    })
 });
