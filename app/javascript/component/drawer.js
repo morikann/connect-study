fb98c547bd6b03ac64f88008da40a7d53b002a86
morikann <mrknt0088@gmail.com>
2021-01-07 16:21:17 +0900

    イベントリスナーのエラー対処

--- app/javascript/component/drawer.js
+++ app/javascript/component/drawer.js
@@ -4,11 +4,11 @@ document.addEventListener('turbolinks:load', () => {
   const body = document.body;
   const documentElement = document.documentElement;
 
-  window.addEventListener('scroll', () => {
+  window?.addEventListener('scroll', () => {
     // スクロール量を取得
     const scrollAmount = documentElement.scrollTop;
     // サイドバーのクリックイベント
-    checkBox.addEventListener('click', (e) => {
+    checkBox?.addEventListener('click', (e) => {
       // サイドバーを開いて閉じた際にスクロール位置を変えないように設定
       documentElement.scrollTop = scrollAmount;
       // サイドバーが開いている際は背景を固定
